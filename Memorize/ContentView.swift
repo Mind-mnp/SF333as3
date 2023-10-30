56bcd6fd7c0965b36c3c38a9e316663adeda2da6
NICHAPA <99460016+Mind-mnp@users.noreply.github.com>
2023-10-30 21:59:18 +0700

    Update ContentView.swift
    Update ContentView and Change color

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isButtonTapped = false
-    let weather_icon = ["☀️", "❄️", "🌧️", "🌈", "🌪️", "☃️", "🎄","🌸","☀️", "❄️", "🌧️", "🌈", "🌪️", "☃️", "🎄","🌸", ].shuffled()
-    let sport_icon = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🏐", "🏉","🎱", "⚽️", "🏀", "🏈", "⚾️", "🥎", "🏐", "🏉","🎱"].shuffled()
-    let vehicles_icon = ["🚗", "⛵️", "🚌", "🏎️", "🛵", "✈️", "🚀","🛸", "🚗", "⛵️", "🚌", "🏎️", "🛵", "✈️", "🚀","🛸",].shuffled()
+    let weather_icon = ["☀️", "❄️", "🌧️", "🌈", "🌪️", "☃️", "🎄","🌸","☀️", "❄️", "🌧️", "🌈", "🌪️", "☃️", "🎄","🌸", ]
+    let sport_icon = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🏐", "🏉","🎱", "⚽️", "🏀", "🏈", "⚾️", "🥎", "🏐", "🏉","🎱"]
+    let vehicles_icon = ["🚗", "⛵️", "🚌", "🏎️", "🛵", "✈️", "🚀","🛸", "🚗", "⛵️", "🚌", "🏎️", "🛵", "✈️", "🚀","🛸",]
 
     @State private var current_theme_icon: [String] = []
     @State var cardCount = 16
@@ -23,39 +23,53 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
+            // Title
             Text("Memorize")
-                .font(.custom("Adelle Sans Devanagari Bold", fixedSize: 45))
-                .foregroundColor(Color(red: 93/255, green: 109/255, blue: 126/255))
-
-            ScrollView {
-                cards
+                .font(.custom("Adelle Sans Devanagari Bold", fixedSize: 40))
+                .padding(.horizontal)
+                .bold()
+                .foregroundColor(Color(red: 252/255, green: 67/255, blue: 89/255))
+            // create card
+            
+            ForEach(0..<4, id: \.self){row in
+                HStack(spacing: 8){
+                    ForEach(0..<4, id: \.self){column in
+                        CardView(content: current_theme_icon[row * 4 + column])
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                .frame(maxHeight: .infinity)
             }
+            .foregroundColor(Color(red: 252/255, green: 67/255, blue: 89/255))
+//            ScrollView {
+//                cards
+//            }
             Spacer()
             cardCountAdjusters
         }
         .padding()
     }
 
-    var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 66))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: current_theme_icon[index])
-                    .aspectRatio(5/7, contentMode: .fit)
-            }
-        }
-        .foregroundColor(Color(red: 93/255, green: 109/255, blue: 126/255))
-    }
+//    var cards: some View {
+//        LazyVGrid(columns: [GridItem(.adaptive(minimum: 66))]) {
+//            ForEach(0..<cardCount, id: \.self) { index in
+//                CardView(content: current_theme_icon[index])
+//                    .aspectRatio(5/7, contentMode: .fit)
+//            }
+//        }
+//        .foregroundColor(Color(red: 93/255, green: 109/255, blue: 126/255))
+//    }
 
     func cardCountAdjuster(by theme: String, symbol: String) -> some View {
         Button(action: {
             currentTheme = theme
             switch theme {
                 case "Weather":
-                    current_theme_icon = weather_icon
+                    current_theme_icon = weather_icon.shuffled()
                 case "Sport":
-                    current_theme_icon = sport_icon
+                    current_theme_icon = sport_icon.shuffled()
                 case "Vehicles":
-                    current_theme_icon = vehicles_icon
+                    current_theme_icon = vehicles_icon.shuffled()
                 default:
                     break
             }
@@ -64,10 +78,10 @@ struct ContentView: View {
                 //Image(systemName: symbol)
                 //Image(currentTheme == theme ? systemName: symbol.fill : systemName: symbol)
                 Image(systemName: currentTheme == theme ? "\(symbol).fill" : symbol)
-                    .foregroundColor(Color(red: 93/255, green: 109/255, blue: 126/255))
+                    .foregroundColor(Color(red: 252/255, green: 67/255, blue: 89/255))
 
                 Text(theme).font(.system(size: 24))
-                    .foregroundColor(Color(red: 93/255, green: 109/255, blue: 126/255))
+                    .foregroundColor(Color(red: 252/255, green: 67/255, blue: 89/255))
             }
         })
     }
@@ -88,7 +102,7 @@ struct ContentView: View {
 struct CardView: View {
     let content: String
     
-    @State var isFaceUp = true
+    @State var isFaceUp = false
 
     var body: some View {
         ZStack {
